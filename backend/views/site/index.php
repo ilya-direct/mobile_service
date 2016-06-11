d8089f51adc54096648f7172db17c7255f9018c2
Ilya Smirnov <ilya-direct@ya.ru>
2016-06-11 13:55:12 +0300

    JS на кнопку "Забыли пароль". Навигация на главной странице в админке

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use yii\helpers\Url;
+
 /**
  * @var $this yii\web\View
  */
@@ -7,6 +9,27 @@ $this->title = 'Мобильная лаборатория - Главная';
 ?>
 <div class="site-index">
     <div class="body-content">
-
+        <div class="list-group">
+            <a href="<?= Url::to(['/content/news']) ?>" class="list-group-item">
+                <h4 class="list-group-item-heading">Новости</h4>
+                <p class="list-group-item-text">Список новостей для сайта</p>
+            </a>
+            <a href="<?= Url::to(['/content/device']) ?>" class="list-group-item">
+                <h4 class="list-group-item-heading">Устройства</h4>
+                <p class="list-group-item-text">Устройства, которые мы ремонтируем с ценами и акциями</p>
+            </a>
+            <a href="<?= Url::to(['/content/category']) ?>" class="list-group-item">
+                <h4 class="list-group-item-heading">Категории</h4>
+                <p class="list-group-item-text">Категории устройств по брендам. Древовидная структура</p>
+            </a>
+            <a href="<?= Url::to(['/settings/admin']) ?>" class="list-group-item">
+                <h4 class="list-group-item-heading">Сотрудники компании</h4>
+                <p class="list-group-item-text">Редактирование профилей сотрудников</p>
+            </a>
+            <a href="<?= Url::to(['/settings/customer']) ?>" class="list-group-item">
+                <h4 class="list-group-item-heading">Клиенты</h4>
+                <p class="list-group-item-text">Список всех клиентов с возможностью добавления новых</p>
+            </a>
+        </div>
     </div>
 </div>
--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -11,6 +11,15 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Авторизация';
 $this->params['breadcrumbs'][] = $this->title;
+
+$onHidden = <<<JS
+function () {
+    $('#modal-msg').text('');
+    $('#forgot-password').show();
+}
+JS;
+
+
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -34,18 +43,49 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php ActiveForm::end(); ?>
 
             <?php \yii\bootstrap\Modal::begin([
-                'header' => Html::tag('h2', 'Восстановление пароля'),
+                'header' => Html::tag('h4', 'Восстановление пароля', ['class' => 'modal-title']),
                 'toggleButton' => [
                     'tag' => 'a',
                     'label' => 'Забыли пароль?',
                 ],
+                'clientEvents' => [
+                    'hidden.bs.modal' => $onHidden,
+                ]
             ]) ?>
-            <?= Html::beginForm(['/site/remember-password'], 'post') ?>
-            <?= Html::input('text', 'email', null, ['class' => 'form-control']) ?>
-            <?= Html::submitButton('Восстановить пароль', ['class' => 'btn bln-primary']) ?>
+
+            <div id="modal-msg"></div>
+
+            <?= Html::beginForm(['/site/remember-password'], 'post', ['id' => 'forgot-password']) ?>
+            <div class="form-group">
+                <label for="recipient-name" class="control-label">Введите свой Email:</label>
+                <?= Html::input('text', 'email', null, ['class' => 'form-control']) ?>
+            </div>
+            <div class="form-group">
+                <?= Html::submitButton('Восстановить пароль', ['class' => 'btn btn-primary']) ?>
+            </div>
             <?php Html::endForm() ?>
             <?php \yii\bootstrap\Modal::end(); ?>
 
         </div>
     </div>
 </div>
+
+<?php
+
+$this->registerJs(<<<JS
+$('#forgot-password').submit(function () {
+    var formData = {'email': $(this).find('input[name=email]')[0].value};
+    $.ajax({
+        url :'/site/remember-password',
+        method : 'post',
+        data: formData,
+        dataType : 'json',
+        success : function(data){
+            $('#forgot-password').hide();
+            $('#modal-msg').text(data.msg);
+        }
+    });
+    return false;
+});
+JS
+);
