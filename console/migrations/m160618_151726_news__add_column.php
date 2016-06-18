f58913c816c34389cb233cbcf550ab8f3d328560
Ilya Smirnov <ilya-direct@ya.ru>
2016-06-18 18:37:17 +0300

    News add column created_by

---
+++ console/migrations/m160618_151726_news__add_column.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+class m160618_151726_news__add_column extends Migration
+{
+    public function safeUp()
+    {
+        $this->addColumn('{{%news}}', 'created_by', $this->integer());
+        $this->addForeignKey(
+            'FK__news__created_by__admin__id',
+            '{{%news}}',
+            'created_by',
+            '{{%admin}}',
+            'id',
+            'RESTRICT',
+            'CASCADE'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('FK__news__created_by__admin__id', '{{%news}}');
+        $this->dropColumn('{{%news}}','created_by');
+    }
+}
