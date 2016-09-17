0f3efc54b1374611e7e4331c8ab309c9a12a2426
Ilya Smirnov <ilya-direct@ya.ru>
2016-09-17 13:03:11 +0300

    Revision transfer to FixController

--- console/controllers/FixController.php
+++ console/controllers/FixController.php
@@ -4,6 +4,7 @@ namespace console\controllers;
 
 use Yii;
 use yii\console\Controller;
+use yii\db\Expression;
 use common\models\ar\Device;
 use common\models\ar\DeviceAssign;
 use common\models\ar\DeviceCategory;
@@ -11,15 +12,22 @@ use common\models\ar\News;
 use common\models\ar\Order;
 use common\models\ar\OrderPerson;
 use common\models\ar\OrderService;
+use common\models\ar\Revision;
+use common\models\ar\RevisionField;
+use common\models\ar\RevisionTable;
+use common\models\ar\RevisionValueType;
 use common\models\ar\Service;
 use common\models\ar\User;
 use common\models\ar\Vendor;
 
 class FixController extends Controller
 {
-    public function actionSequence()
+    protected $tables = [];
+    protected $consoleUserId;
+
+    public function init()
     {
-        $tables = [
+        $this->tables = [
             Device::className(),
             DeviceAssign::className(),
             DeviceCategory::className(),
@@ -32,12 +40,85 @@ class FixController extends Controller
             Vendor::className(),
         ];
 
-        foreach ($tables as $table) {
+        $user = User::findOne(['email' => 'console@console.ru']);
+        if (is_null($user)) {
+            $user = (new User([
+                'first_name' => 'console',
+                'last_name' => 'console',
+                'auth_key' => '',
+                'email' => 'console@console.ru',
+                'phone' => '+77777777777',
+                'enabled' => false,
+            ]));
+            $user->save(false);
+        }
+
+        $this->consoleUserId = $user->id;
+    }
+
+    /**
+     * Исправляет значения последовательностей, делая их максимальными
+     * @throws \yii\db\Exception
+     */
+    public function actionSequence()
+    {
+        foreach ($this->tables as $table) {
             $tableName = $table::getTableSchema()->name;
             $column = 'id';
             Yii::$app->db->createCommand("SELECT setval('{$tableName}_{$column}_seq',( SELECT MAX([[$column]]) FROM {$tableName}))")->execute();
         }
 
+    }
+
+
+    /**
+     * Создаёт начальную ревизию всех полей, которые не были проинициализированны
+     *
+     * @throws \yii\db\Exception
+     */
+    public function actionRevision()
+    {
+        foreach ($this->tables as $table) {
+
+            $models = $table::find()->all();
+            foreach ($models as $model) {
+                if (!isset($model->behaviors['revision'])) {
+                    continue;
+                }
+                $revisionTableId = RevisionTable::findOrCreateReturnScalar(['name' => $table::getTableSchema()->name]);
+                $attributes = $model->behaviors['revision']->attributes;
+
+                $revisionTableName = Revision::tableName();
+                $versionedFields = Revision::find()
+                    ->select( RevisionField::tableName() . '.name')
+                    ->joinWith('revisionField')
+                    ->where([
+                        $revisionTableName . '.revision_table_id' => $revisionTableId,
+                        $revisionTableName . '.operation_type' => Revision::OPERATION_INSERT,
+                        $revisionTableName . '.record_id' => $model->id,
+                        RevisionField::tableName() . '.name' => $attributes,
+                    ])
+                    ->distinct()
+                    ->column();
+
+                $notVersionedFields = array_diff($attributes, $versionedFields);
+
+                foreach ($notVersionedFields as $field) {
+                    Yii::$app->db->createCommand()
+                        ->insert($revisionTableName, [
+                                'revision_table_id' => $revisionTableId,
+                                'revision_field_id' => RevisionField::findOrCreateReturnScalar(['name' => $field]),
+                                'record_id' => $model->id,
+                                'revision_value_type_id' => RevisionValueType::findOrCreateReturnScalar(['name' => gettype($model->{$field})]),
+                                'value' => $model->{$field},
+                                'user_id' => $this->consoleUserId,
+                                'operation_type' => Revision::OPERATION_INSERT,
+                                'created_at' => new Expression('NOW()'),
+                            ]
+                        )->execute();
+                }
+            }
+        }
     }
     
 }
